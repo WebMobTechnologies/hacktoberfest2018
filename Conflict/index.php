d518fff576b95fe6e8c004d8b01f30985b9ba2c2
kamerk22 <kashyapk62@gmail.com>
2018-10-19 15:46:24 +0530

    conflict

--- Conflict/index.php
+++ Conflict/index.php
@@ -5,16 +5,20 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Documenasdasdt</title>
+    <title>Documenaasdasdasdt</title>
+    <style></style>
+    <style></style>
+<style></style>
 </head> 
-<body class="conflict">
+<body class="confliccccct">
 
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
+<div class="asdas">
+adsadading</h1>
+<p>This is a paragsadasdraph.</p>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicinzxcxzcxzg elit. Beatae ducimus omnis reiciendis? Aliquam aliquid doloremque illo in inventore molestias nam praesentium. A asperiores iusto nostrum odit quod ratione rerum sed! A ad blanditiis commodi cumque distinctio ducimus est exercitationem fuga harum ipsam iure, laboriosam laudantium minus quibusdam reiciendis totam voluptate.</p>
 Lorem ipsum dolor sit amet, conzxczxcur adipisicing elit. Ad assumenda debitis, dolores et fugiat id molestias necessitatibus reiciendis sed soluta sunt, totam ut voluptate. Autem dignissimos inventore ipsam itaque nulla.
-Lorem ipsum dolor sit amet, consectezxczxctur adipisicing elit. Ad assum
-
+Lorem ipsum dolasdasdor sit amet, consectezxczxctur adipisicing elit. Ad assum
+</div>
 enda debitis, dolores et fugiat id molestias necessitatibus reiciendis sed soluta sunt, totam ut voluptate. Autem dignissimos inventore ipsam itaque nulla.enda debitis, dolores et fugiat id molestias necessitatibus reiciendis sed soluta sunt, totam ut voluptate. Autem dignissimos inventore ipsam itaque nulla.enda debitis, dolores et fugiat id molestias necessitatibus reiciendis sed soluta sunt, totam ut voluptate. Autem dignissimos inventore ipsam itaque nulla.
 
 </body>
