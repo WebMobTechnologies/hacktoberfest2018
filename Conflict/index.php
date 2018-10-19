bc33ff4825ecce7c2df731941a355b2ce7e82a08
kamerk22 <kashyapk62@gmail.com>
2018-10-19 15:49:09 +0530

    conflict

--- Conflict/index.php
+++ Conflict/index.php
@@ -1,25 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Documenaasdasdasdt</title>
-    <style></style>
-    <style></style>
-<style></style>
-</head> 
-<body class="confliccccct">
+<?php
 
-<div class="asdas">
-adsadading</h1>
-<p>This is a paragsadasdraph.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicinzxcxzcxzg elit. Beatae ducimus omnis reiciendis? Aliquam aliquid doloremque illo in inventore molestias nam praesentium. A asperiores iusto nostrum odit quod ratione rerum sed! A ad blanditiis commodi cumque distinctio ducimus est exercitationem fuga harum ipsam iure, laboriosam laudantium minus quibusdam reiciendis totam voluptate.</p>
-Lorem ipsum dolor sit amet, conzxczxcur adipisicing elit. Ad assumenda debitis, dolores et fugiat id molestias necessitatibus reiciendis sed soluta sunt, totam ut voluptate. Autem dignissimos inventore ipsam itaque nulla.
-Lorem ipsum dolasdasdor sit amet, consectezxczxctur adipisicing elit. Ad assum
-</div>
-enda debitis, dolores et fugiat id molestias necessitatibus reiciendis sed soluta sunt, totam ut voluptate. Autem dignissimos inventore ipsam itaque nulla.enda debitis, dolores et fugiat id molestias necessitatibus reiciendis sed soluta sunt, totam ut voluptate. Autem dignissimos inventore ipsam itaque nulla.enda debitis, dolores et fugiat id molestias necessitatibus reiciendis sed soluta sunt, totam ut voluptate. Autem dignissimos inventore ipsam itaque nulla.
+class Conflict {
 
-</body>
-</html>
+    public function x() {
+        return "okay";
+    }
+}
+?>
